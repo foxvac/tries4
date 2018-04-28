f80cd1c8576da548f2b76dec32d0fa96e1f1f3ef
foxvac <38643902+foxvac@users.noreply.github.com>
2018-04-28 20:18:23 +0800

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -5,11 +5,11 @@ echo "请输入你的内网ip"
 read -p "内网ip： " ip
 cd
 cd libpcap-1.8.1
-rm -rf tries/
+rm -rf tries2/
 cd 
-rm -rf tries/
+rm -rf tries2/
 chmod +x restart.sh
-git clone https://github.com/foxvac/tries.git
+git clone https://github.com/foxvac/tries2.git
 cd tries/
 npm i
 npm i -g pino
